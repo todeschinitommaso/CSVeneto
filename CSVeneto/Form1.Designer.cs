6ccfd0cc3ada133c84cddb496bc4b2bc5c1d8936
Tommaso Todeschini <tommaso.todeschini05@gmail.com>
2022-10-02 19:25:59 +0200

    Aggiungere i file di progetto.

---
+++ CSVeneto/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CSVeneto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.namebox = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.searchbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // namebox
+            // 
+            this.namebox.AutoSize = true;
+            this.namebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namebox.Location = new System.Drawing.Point(317, 102);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(24, 24);
+            this.namebox.TabIndex = 0;
+            this.namebox.Text = "X";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(61, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NOME COMUNE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(317, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "HOTEL";
+            // 
+            // searchbutton
+            // 
+            this.searchbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchbutton.Location = new System.Drawing.Point(119, 148);
+            this.searchbutton.Name = "searchbutton";
+            this.searchbutton.Size = new System.Drawing.Size(95, 32);
+            this.searchbutton.TabIndex = 4;
+            this.searchbutton.Text = "CERCA";
+            this.searchbutton.UseVisualStyleBackColor = true;
+            this.searchbutton.Click += new System.EventHandler(this.searchbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 192);
+            this.Controls.Add(this.searchbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.namebox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label namebox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button searchbutton;
+    }
+}
+
---
+++ CSVeneto/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSVeneto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchbutton_Click(object sender, EventArgs e)
+        {
+            string cerca = textBox1.Text.ToUpper();
+            namebox.Text = (Ricerca(filename, cerca));
+        }
+
+        public string filename = "veneto_verona.csv";
+
+        static string Ricerca(string filename, string nomecercato)
+        {
+            string line;
+            var f = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite); //accesso al file binario 
+            BinaryReader reader = new BinaryReader(f);
+            BinaryWriter writer = new BinaryWriter(f);
+
+            int righetot = Convert.ToInt32(f.Length); //byte totali
+            int lunghezzariga = 528;
+            righetot /= 528; //numero record
+
+            string result;
+
+            int lung = Convert.ToInt32(f.Length);
+            int i = 0, j = righetot - 1, m, pos = -1;
+
+            do //ricerca dicotomica
+            {
+                m = (i + j) / 2;
+                f.Seek(m * lunghezzariga, SeekOrigin.Begin);
+                line = Encoding.ASCII.GetString(reader.ReadBytes(lunghezzariga));
+                result = FromString(line, 0);
+
+                if (myCompare(result, nomecercato) == 0)
+                {
+                    pos = m;
+                }
+
+                else if (myCompare(result, nomecercato) == -1)
+                {
+                    i = m + 1;
+                }
+
+                else
+                {
+                    j = m - 1;
+                }
+
+
+            } while (i <= j && pos == -1);
+
+            if (pos == -1)
+            {
+                throw new Exception("campo non trovato");
+            }
+               
+            string fine = FromString(line, 7);
+            f.Close();
+            return fine;
+
+        }
+
+        static int myCompare(string stringa1, string stringa2)
+        {
+            if (stringa1 == stringa2) //0=uguali 1=stringa prima -1=stringa dopo
+            {
+                return 0;
+            }
+
+            char[] char1 = stringa1.ToCharArray();
+            char[] char2 = stringa2.ToCharArray();
+            int l = char1.Length;
+
+            if (char2.Length < l) //l = lunghezza minore
+            {
+                l = char2.Length;
+            }
+
+            for (int i = 0; i < l; i++)
+            {
+                if (char1[i] < char2[i])
+                {
+                    return -1;
+                }
+
+                if (char1[i] > char2[i])
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+        public static string FromString(string Stringa, int pos, string sep = ";") //separa i campi
+        {
+            string[] ris = Stringa.Split(';');
+            return ris[pos];
+
+        }
+
+    }
+}
